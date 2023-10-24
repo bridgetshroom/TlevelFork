9cb0a713d15f667f706f569e53f3fa081f9f20d5
Paul Smith <three_terriers@btinternet.com>
2023-10-24 13:10:52 +0000

    Added subtract function

--- mathTL.py
+++ mathTL.py
@@ -29,10 +29,34 @@ class MathTL():
 
         try:
             n1 = float(n1)
-        except:
+        except TypeError:
             n1 = 0
        
         return n1 + n2
 
+    @staticmethod
+    def sub(n1:float,n2:float) -> float:
+        '''
+        This is a demonstration method only, subtracting 2 numbers
+        Behaviour for other datatypes is not implimented
+        Submitted by Paul Smith
 
+        Parameters
+        ----------
+        n1 : float
+            The minuend
+        n2: float
+            The subtrahend
 
+        Returns
+        -------
+        float
+            The Sum of the addends
+        '''
+
+        try:
+            n1 = float(n1)
+        except TypeError:
+            n1 = 0
+
+        return n1 - n2
